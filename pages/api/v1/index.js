68d6771b6bba6fa9ed4f8ee76a94053f6d408a5e
Abhin4vKumar <68866591+Abhin4vKumar@users.noreply.github.com>
2023-11-29 02:30:38 +0530

    Delete pages/api/v1 directory

--- pages/api/v1/index.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const mongoose = require('mongoose');
-import {connectDatabase , closeConnection} from "@/utils/database"
-export default async function handler(req, res) {
-  if(req.method == 'GET'){
-    console.log("GET");
-  }
-  res.status(200).json({HeyDev:"Welcome to the v1 Api of BlogPost"})
-}
