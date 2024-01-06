fe071e872a19e03e244545c882853febfb912ff1
Abhinav Kumar <abhinavkumar1312004@gmail.com>
2024-01-06 11:22:19 +0530

    change BaseURL and MongoDB

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -1,4 +1,4 @@
-export const baseURL = 'blog-site-server-gold.vercel.app';
+export const baseURL = 'https://blog-site-server-gold.vercel.app';
 export const postOptions = {
     method:"POST",
     headers:{"Content-Type":"application/json"},
